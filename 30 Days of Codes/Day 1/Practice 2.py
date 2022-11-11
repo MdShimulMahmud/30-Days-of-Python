55d3c3499f6345a5eccfbefc0132322b83dc7eab
Shimul Mahmud <shimulmahmud18@gmail.com>
2022-11-11 16:23:05 +0600

    added

--- 30 Days of Codes/Day 1/Practice 2.py
+++ 30 Days of Codes/Day 1/Practice 2.py
@@ -1,8 +1,14 @@
 import math
 
+#problem 1
 a = int(input())
 b = int(input())
 c = int(input())
 x1 = ((-b) + (math.sqrt(b**2 - 4*a*c))) / (2*a)
 x2 = ((-b) -( math.sqrt(b**2 - 4*a*c)))/ (2*a)
 print(int(x1), int(x2))
+
+#problem 2
+c = input("Enter celcius: ")
+answer = ((9/5)*float(c))+32
+print(answer)
